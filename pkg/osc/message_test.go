fea1d47085934eced92486c3faf9db41ddd7f359
Joel Wetzell <jwetzell@yahoo.com>
2024-12-22 13:38:48 -0600

    cleanup comments

--- pkg/osc/message_test.go
+++ pkg/osc/message_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
 func TestOSCMessageEncoding(t *testing.T) {
 
 	testCases := []struct {
